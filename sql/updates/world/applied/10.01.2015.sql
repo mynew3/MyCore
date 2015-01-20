559082a6bb3adffb188fd513bd5abc6da879e745
Shuni <wowa.ru29@gmail.com>
2015-01-20 22:15:54 +0300

    *Исправление кодировки

--- sql/updates/world/applied/10.01.2015.sql
+++ sql/updates/world/applied/10.01.2015.sql
@@ -1,4 +1,4 @@
--- Небольшой фикс
+-- РќРµР±РѕР»СЊС€РѕР№ С„РёРєСЃ
 REPLACE INTO `spell_linked_spell` VALUES 
 (63644, -58427, 0, 'Overkill remove when change talents'),
 (63644, -31665, 0, 'Master of Subt. remove when change talents'),
