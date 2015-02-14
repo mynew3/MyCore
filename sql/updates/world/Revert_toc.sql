263f0cee5054b3adb999e3957fec7367bc65f8c1
Shuni <wowa.ru29@gmail.com>
2015-02-14 22:04:31 +0300

    *Добавил забытые данные.

---
+++ sql/updates/world/Revert_toc.sql
@@ -0,0 +1,142 @@
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33217';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33316';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33317';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33318';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33319';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33320';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33321';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33322';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33323';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags2` = '2048',`Health_mod` = '3.96825',`mechanic_immune_mask` = '0',`ScriptName` = '' WHERE `entry` = '33324';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_hunter_toc5' WHERE `entry` = '34657';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '34658';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_shaman_toc5' WHERE `entry` = '34701';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_mage_toc5' WHERE `entry` = '34702';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_rouge_toc5' WHERE `entry` = '34703';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_warrior_toc5' WHERE `entry` = '34705';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`mechanic_immune_mask` = '0',`ScriptName` = 'boss_paletress' WHERE `entry` = '34928';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '34942';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'npc_announcer_toc5' WHERE `entry` = '35004';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'npc_announcer_toc5' WHERE `entry` = '35005';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35028';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35029';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35030';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35031';
+UPDATE `creature_template` SET `minlevel` = '1',`maxlevel` = '1',`exp` = '0',`faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35032';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35033';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35034';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35036';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35037';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35038';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35039';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35040';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35041';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35042';
+UPDATE `creature_template` SET `minlevel` = '1',`maxlevel` = '1',`exp` = '0',`faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35043';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35044';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35045';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35046';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35047';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35048';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = 'npc_memory' WHERE `entry` = '35049';
+UPDATE `creature_template` SET `minlevel` = '1',`maxlevel` = '1',`exp` = '0',`faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35050';
+UPDATE `creature_template` SET `minlevel` = '1',`maxlevel` = '1',`exp` = '0',`faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35051';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35052';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`mechanic_immune_mask` = '0',`ScriptName` = 'boss_eadric' WHERE `entry` = '35119';
+UPDATE `creature_template` SET `difficulty_entry_1` = '35306',`unit_flags2` = '2048',`ScriptName` = 'npc_argent_soldier' WHERE `entry` = '35305';
+UPDATE `creature_template` SET `difficulty_entry_1` = '35308',`unit_flags2` = '2048',`ScriptName` = 'npc_argent_soldier' WHERE `entry` = '35307';
+UPDATE `creature_template` SET `difficulty_entry_1` = '35310',`unit_flags2` = '2048',`ScriptName` = 'npc_argent_soldier' WHERE `entry` = '35309';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35314';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35323';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35325';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35326';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35327';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35328';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35329';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35330';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35331';
+UPDATE `creature_template` SET `unit_flags` = '0',`unit_flags2` = '2048',`AIName` = 'PassiveAI' WHERE `entry` = '35332';
+UPDATE `creature_template` SET `unit_flags` = '320',`unit_flags2` = '2048',`mechanic_immune_mask` = '8388624',`ScriptName` = 'boss_black_knight' WHERE `entry` = '35451';
+UPDATE `creature_template` SET `faction_H` = '14',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`dmg_multiplier` = '7.5',`baseattacktime` = '2000',`unit_flags` = '0',`unit_flags2` = '2048',`dynamicflags` = '8',`minrangedmg` = '345',`maxrangedmg` = '509',`rangedattackpower` = '103',`VehicleId` = '107',`ScriptName` = 'npc_black_knight_skeletal_gryphon' WHERE `entry` = '35491';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = '' WHERE `entry` = '35492';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'npc_risen_ghoul' WHERE `entry` = '35545';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'npc_risen_ghoul' WHERE `entry` = '35564';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_mage_toc5' WHERE `entry` = '35569';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_hunter_toc5' WHERE `entry` = '35570';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_shaman_toc5' WHERE `entry` = '35571';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_warrior_toc5' WHERE `entry` = '35572';
+UPDATE `creature_template` SET `unit_flags2` = '2048',`ScriptName` = 'boss_rouge_toc5' WHERE `entry` = '35617';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35633';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1' WHERE `entry` = '35634';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35635';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35636';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35637';
+UPDATE `creature_template` SET `scale` = '1',`rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35638';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35640';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35641';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`speed_run` = '1.57143',`baseattacktime` = '2000',`unit_flags2` = '2048',`spell1` = '68505',`spell3` = '68282',`VehicleId` = '529' WHERE `entry` = '35644';
+UPDATE `creature_template` SET `rank` = '0',`mindmg` = '422',`maxdmg` = '586',`attackpower` = '642',`unit_flags` = '33587456',`unit_flags2` = '2048',`Health_mod` = '1',`ScriptName` = 'generic_vehicleAI_toc5' WHERE `entry` = '35768';
+UPDATE `creature_template` SET `faction_A` = '35',`faction_H` = '35',`speed_run` = '1.57143',`baseattacktime` = '2000',`unit_flags` = '0',`unit_flags2` = '2048',`spell1` = '0',`spell2` = '0',`spell3` = '0',`spell4` = '0' WHERE `entry` = '36558';
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = '35590';
+
+UPDATE `instance_template` SET `Script` = 'instance_trial_of_the_champion' WHERE `map` = 650;
+
+DELETE FROM `vehicle_template_accessory` WHERE `entry` 
+IN (35638,35637,35635,34658,35636,35768,35633,35640,35641,35634,33321,33323,33320,
+33322,33316,33317,33318,33319,35491);
+INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
+('35637','34705','0','0','Marshal Jacob Alerius\' Mount','6','30000'),
+('35633','34702','0','0','Ambrose Boltspark\'s Mount','6','30000'),
+('35768','34701','0','0','Colosos\' Mount','6','30000'),
+('34658','34657','0','0','Jaelyne Evensong\'s Mount','6','30000'),
+('35636','34703','0','0','Lana Stouthammer\'s Mount','6','30000'),
+('35638','35572','0','0','Mokra the Skullcrusher\'s Mount','6','30000'),
+('35635','35569','0','0','Eressea Dawnsinger\'s Mount','6','30000'),
+('35640','35571','0','0','Runok Wildmane\'s Mount','6','30000'),
+('35641','35570','0','0','Zul\'tore\'s Mount','6','30000'),
+('35634','35617','0','0','Deathstalker Visceri\'s Mount','6','30000');
+
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN (35637,35633,35768,34658,35636,35638,35635,35640,35641,35634,33321,33323,33320,33322,33316,33217,33324,33317,33318,33319,35491);
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES
+('33217','63151','1','0'),
+('33316','63151','1','0'),
+('33317','63151','1','0'),
+('33318','63151','1','0'),
+('33319','63151','1','0'),
+('33320','63151','1','0'),
+('33321','63151','1','0'),
+('33322','63151','1','0'),
+('33323','63151','1','0'),
+('33324','63151','1','0'),
+('34658','46598','1','0'),
+('35633','46598','1','0'),
+('35634','46598','1','0'),
+('35635','46598','1','0'),
+('35636','46598','1','0'),
+('35637','46598','1','0'),
+('35638','46598','1','0'),
+('35640','46598','1','0'),
+('35641','46598','1','0'),
+('35768','46598','1','0');
+
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (12302, 12303, 12304, 12305, 12306, 12307, 12308, 12318, 12319, 12320, 12321, 12322, 12323, 12324, 12439, 11420, 12298, 12299, 12300, 12301, 11559, 11560, 12310, 12311, 12312, 12313, 12314, 12315, 12316, 12317);
+
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (67680, 66903, 66515);
+
+-- Horde announcer
+DELETE FROM `creature_text` WHERE `entry` = 35004 AND `groupid` BETWEEN 20 AND 27;
+-- Ally announcer
+DELETE FROM `creature_text` WHERE `entry` = 35005 AND `groupid` BETWEEN 20 AND 27;
+-- Jaina Proudmoore
+DELETE FROM `creature_text` WHERE `entry` = 34992 AND `groupid` IN (10, 11);
+-- Varian
+UPDATE `creature_text` SET `type` = 12 WHERE `groupid` IN (50, 52) AND `entry` = 34990;
+
+UPDATE `gameobject_template` SET `flags` = `flags` |16 WHERE `entry` IN (195324, 195323, 195375, 195374, 195710, 195709);
+
+-- Condition for Hammer of the Righteous
+DELETE FROM `conditions` WHERE `SourceEntry` = 66905 AND `SourceTypeOrReferenceId` = 13;
+
+-- Some Argent Champion achievements criterias
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (11863, 11904, 11905, 11906, 11907, 11908, 11909, 11910, 11911,
+11912, 11913, 11914, 11915, 11916, 11917, 11918, 11919, 11920, 11921, 11922, 11923, 11924, 11925, 11926, 11927, 11858);
