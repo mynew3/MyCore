8b1bfe17f1e427a988136611b8371eebe271922d
Shuni <wowa.ru29@gmail.com>
2015-02-21 02:53:06 +0300

    *Добавил скулы относящиеся к логам гмов

--- sql/updates/characters/gm_logy_item.sql
+++ sql/updates/characters/gm_logy_item.sql
@@ -1,14 +1,16 @@
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `gm_logy`;
-CREATE TABLE `gm_logy` (
+DROP TABLE IF EXISTS `gm_logy_item`;
+CREATE TABLE `gm_logy_item` (
   `id` int(9) NOT NULL AUTO_INCREMENT,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `guid` int(9) NOT NULL,
   `player` varchar(12) CHARACTER SET latin1 NOT NULL,
   `account` int(9) NOT NULL,
-  `command` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `item` int(6) NOT NULL,
+  `item_guid` int(6) NOT NULL,
+  `state` tinyint(1) NOT NULL,
   `position` varchar(96) CHARACTER SET latin1 NOT NULL,
-  `selected` varchar(96) CHARACTER SET latin1 NOT NULL,
+  `target` varchar(96) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `account` (`account`),
-  KEY `player` (`player`)
+  KEY `guid` (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
