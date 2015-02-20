75eb34c53f6fc616196fb16b332f8a0570b03554
Shuni <wowa.ru29@gmail.com>
2015-02-20 14:51:50 +0300

    *Добавление профера в ветку х100 и подгонка под стандарты.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -690,8 +690,6 @@ void AddSC_outdoorpvp_zm();
 void AddSC_chat_log();
 void AddSC_action_ip_logger();
 
-void AddSC_PWS_Transmogrification();
-void AddSC_CS_Transmogrification();
 #endif
 
 void AddScripts()
@@ -1422,19 +1420,14 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
     void AddSC_custom();
-	void AddSC_DuelReset();
-	void AddSC_TeLe_gossip_codebox();
+
 #endif
 
 void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
-     AddSC_DuelReset();
 	 AddSC_custom();
-	 AddSC_TeLe_gossip_codebox();
-	 AddSC_custom();
-	 AddSC_PWS_Transmogrification();
-     AddSC_CS_Transmogrification();
+
 #endif
 }
--- src/server/scripts/Custom/custom.cpp
+++ src/server/scripts/Custom/custom.cpp
@@ -5,7 +5,11 @@ void AddSC_custom_scripts();
 void AddSC_EC_boss_thorim();
 void AddSC_EC_boss_apothecary_trio();
 void AddSC_EC_pilgrim();
-
+void AddSC_Professions_NPC();
+void AddSC_DuelReset();
+void AddSC_TeLe_gossip_codebox();
+void AddSC_PWS_Transmogrification();
+void AddSC_CS_Transmogrification();
 /* This is where custom scripts should be added. */
 void AddSC_custom()
 {
@@ -14,5 +18,10 @@ void AddSC_custom()
     AddSC_EC_boss_thorim();
     AddSC_EC_pilgrim();
 	AddSC_EC_boss_apothecary_trio();
+	AddSC_Professions_NPC();
+	AddSC_DuelReset();
+	AddSC_TeLe_gossip_codebox();
+    AddSC_PWS_Transmogrification();
+    AddSC_CS_Transmogrification();
  
 }
