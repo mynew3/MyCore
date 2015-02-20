3a690369c3ad63df54e3836eeb354b84e5978cd9
Shuni <wowa.ru29@gmail.com>
2015-02-20 15:36:52 +0300

    *Исправление дабл лута в истах.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -299,7 +299,7 @@ bool Creature::InitEntry(uint32 entry, CreatureData const* data /*= nullptr*/)
 
     // Initialize loot duplicate count depending on raid difficulty
     if (GetMap()->Is25ManRaid())
-        loot.maxDuplicates = 3;
+        loot.maxDuplicates = 1;
 
     SetEntry(entry);                                        // normal entry always
     m_creatureInfo = cinfo;                                 // map mode related always
