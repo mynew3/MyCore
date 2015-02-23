8ffcd421a4d853acc075cf348578694e7c041a6c
Shuni <wowa.ru29@gmail.com>
2015-02-23 03:35:12 +0300

    *Исправление мелких багов.

--- sql_x100/updates/world/Fix_enrage.sql
+++ sql_x100/updates/world/Fix_enrage.sql
@@ -7,4 +7,14 @@ DELETE FROM `spell_group` WHERE `id`=@GUID;
 INSERT INTO `spell_group`(`id`,`spell_id`) VALUES
 (@GUID,14204),
 (@GUID,57516),
-(@GUID,57522);
+(@GUID,57522),
+(@GUID,12317),
+(@GUID,13045),
+(@GUID,13046),
+(@GUID,13047),
+(@GUID,13048),
+(@GUID,46867),
+(@GUID,56611),
+(@GUID,55612),
+(@GUID,56613),
+(@GUID,56614);
