635463e886a19664e63093afa80060b5b07eda05
Eldro <eldrogaming@gmail.com>
2015-03-01 01:08:08 +0400

    *Исправление Реликвии Титанов (Оло, Берег Древних) *Перенесение установленных SQL

---
+++ sql/updates/world/01.03.2015.sql
@@ -0,0 +1,5 @@
+UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=192549; -- Chamber of Ancient Relics
+UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=192834; -- Titan Relic
+UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=194083; -- Titan Relic
+UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=192829; -- Titan Relic
+UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=194082; -- Titan Relic
